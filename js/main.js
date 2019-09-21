ca4abab51983ce7e54cbd7e3d830f131f1c0e94d
Sebastian Jimenez <sbstn.jmnz@yahoo.com>
2019-09-21 14:23:23 -0300

    cname added

--- js/main.js
+++ js/main.js
@@ -29,14 +29,13 @@ $(document).ready(function(){
 
 
 	// google map
-		var map;
-		function initMap() {
-		  map = new google.maps.Map(document.getElementById('map'), {
-		    center: {lat: -34.397, lng: 150.644},
-		    zoom: 8
-		  });
-		}
-
+		// var map;
+		// function initMap() {
+		//   map = new google.maps.Map(document.getElementById('map'), {
+		//     center: {lat: -34.397, lng: 150.644},
+		//     zoom: 8
+		//   });
+		// }
 
 	// Counter
 
